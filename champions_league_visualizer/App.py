73435501c355ba584ad329560509c687a3780dad
arnie12a <38790024+arnie12a@users.noreply.github.com>
2022-06-26 11:57:07 -0500

    Add files via upload

---
+++ champions_league_visualizer/App.py
@@ -0,0 +1,27 @@
+from flask import Flask, render_template
+import requests
+from bs4 import BeautifulSoup as bs
+import pandas as pd
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    url = 'https://www.sportingnews.com/us/soccer/news/uefa-champions-league-group-standings-results-match-schedule/z7rxlzuxzlb01kl1ilcra9fz7'
+
+    page = requests.get(url)
+
+    soup = bs(page.content, 'html.parser')
+
+    tables = soup.findAll("table", {"class": "table-retro-standard"})
+
+    all_tables = []
+    for table in tables:
+        all_tables.append(pd.read_html(str(table))[0])
+
+    return render_template('index.html', group_tables=all_tables)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
