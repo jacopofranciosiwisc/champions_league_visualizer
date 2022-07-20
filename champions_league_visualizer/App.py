0bd606bfe7b07a0f757e12a9d1cfaf44b7d9a653
arnie12a <38790024+arnie12a@users.noreply.github.com>
2022-07-19 19:15:51 -0500

    rendered knockout stage to come from csv file

--- champions_league_visualizer/App.py
+++ champions_league_visualizer/App.py
@@ -10,6 +10,7 @@ app = Flask(__name__)
 def index():
     df = pd.read_csv(
         'champions_league_visualizer\data\groupStage2022.csv', header=0)
+
     df = df.rename(columns={'Unnamed: 0': 'Place'})
     df['Place'] = df['Place'] + 1
 
@@ -22,8 +23,12 @@ def index():
             team = team[:-4]
         new_teams.append(team)
     df['Team'] = new_teams
+    ks_df = pd.read_csv(
+        'champions_league_visualizer/data/knockoutStage2022.csv')
+    ks_df['total_Tm1'] = ks_df[' Gm1Tm1'] + ks_df[' Gm2Tm1']
+    ks_df['total_Tm2'] = ks_df[' Gm1Tm2'] + ks_df[' Gm2Tm2']
 
-    return render_template('index.html', table=df, group_teams=new_teams)
+    return render_template('index.html', table=df, group_teams=new_teams, knockout=ks_df)
 
 
 if __name__ == "__main__":
