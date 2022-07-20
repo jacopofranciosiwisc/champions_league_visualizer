7d98092c81018a61c76a0b41076bc673ca8a0529
Jacopo Franciosi <jfranciosi@wisc.edu>
2022-07-19 20:15:28 -0500

    commiting image

--- champions_league_visualizer/App.py
+++ champions_league_visualizer/App.py
@@ -2,18 +2,24 @@ from flask import Flask, render_template
 import requests
 from bs4 import BeautifulSoup as bs
 import pandas as pd
+import os
+
+DATA_FOLDER = os.path.join('data', 'tourney-template')
 
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = DATA_FOLDER
 
 @app.route('/')
 def home():
     return "Welcome to the Champions League Visualizer!"
 
 
+
 @app.route('/2022')
 def index():
-    df = pd.read_csv(
-        'champions_league_visualizer\data\groupStage2022.csv', header=0)
+    tournament_template = os.path.join(app.config['UPLOAD_FOLDER'], 'tournament-template.jpg')
+
+    df = pd.read_csv('champions_league_visualizer\data\groupStage2022.csv', header=0)
     df = df.rename(columns={'Unnamed: 0': 'Place'})
     df['Place'] = df['Place'] + 1
 
@@ -27,7 +33,7 @@ def index():
         new_teams.append(team)
     df['Team'] = new_teams
 
-    return render_template('index.html', table=df, group_teams=new_teams)
+    return render_template('index.html', table=df, group_teams=new_teams, user_image = tournament_template)
 
 
 if __name__ == "__main__":
