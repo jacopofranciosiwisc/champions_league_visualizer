b46b37501b7c65604316ef4b5927e7cf1efe3afc
Jacopo Franciosi <jfranciosi@wisc.edu>
2022-08-28 17:59:49 -0500

    Reorganizing and 'prettyfying'...

--- champions_league_visualizer/App.py
+++ champions_league_visualizer/App.py
@@ -10,12 +10,12 @@ app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = IMG_FOLDER
 
 @app.route('/')
-def home():
-    return "Welcome to the Champions League Visualizer"
+def index():
+    return render_template('index.html')
 
 
 @app.route('/2022')
-def index():
+def yr2022():
     tournament_template = os.path.join(app.config['UPLOAD_FOLDER'], 'tournament-template.jpg')
 
     df = pd.read_csv('champions_league_visualizer\data\groupStage2022.csv', header=0)
@@ -32,7 +32,7 @@ def index():
         new_teams.append(team)
     df['Team'] = new_teams
 
-    return render_template('index.html', table=df, group_teams=new_teams, user_image = tournament_template)
+    return render_template('2022.html', table=df, group_teams=new_teams, user_image = tournament_template)
 
 
 if __name__ == "__main__":
